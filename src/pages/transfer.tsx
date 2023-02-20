44a46572493c867b03e2ab96f22f075f0561ba59
Viktor <viktorbilokapicm@gmail.com>
2023-02-21 00:52:21 +0100

    Zapocet tickets

---
+++ src/pages/transfer.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/rents/Navbar";
+import DenseTable from "@/components/transfer/Timetable";
+import React from "react";
+
+const transfer = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default transfer;
